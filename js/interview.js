00d9a9eba9cf7b1af5e7966bd599b4bbf78cbb2a
bm8546 <lbm8546@gamil.com>
2025-08-11 16:35:36 +0900

    스크롤업 버튼 제작

--- js/interview.js
+++ js/interview.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // 인터뷰 토픽 토글 열고 닫기
   const toggleBtn = document.querySelector(".topics-toggle");
   const list = document.querySelector(".interview-topics ul");
 
@@ -10,5 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toggleBtn.textContent = "목차 보기 ▼";
     }
+  });
+
+  // 플로팅 버튼 스크롤 이벤트 등록
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 });
